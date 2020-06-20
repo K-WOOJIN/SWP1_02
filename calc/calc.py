3a23fa669bcc3ac5f812564b3bb1b39cb0841b23
K-WOOJIN <kwooj2788@kookmin.ac.kr>
2020-06-20 20:31:58 +0900

    issue #6 CHANGE calc.py can input (-) (LOCAL)

--- calc/calc.py
+++ calc/calc.py
@@ -1,12 +1,20 @@
 from cgi import parse_qs
 from template import html
 
+def isInt(num) :
+    try :
+	tmp = int(num)
+	return True
+    except ValueError :
+	return False
+
+
 def application(environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
     x = d.get('x', [''])[0]
     y = d.get('y', [''])[0]
     sum, mult = "No INPUT", "No INPUT"
-    if x.isdigit() and y.isdigit() :
+    if isInt(x) and isInt(y) :
 	x, y = int(x), int(y)
 	sum, mult = x + y, x * y
     response_body = html % {
@@ -18,4 +26,3 @@ def application(environ, start_response):
         ('Content-Length', str(len(response_body)))
     ])
     return [response_body]
-
